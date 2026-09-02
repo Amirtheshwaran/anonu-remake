ae1e24e19966d9d492b714598e78acca3b8f08eb
Amirtheshwaran Navaneethakrishnan <anavane1@charlotte.edu>
2026-09-02 19:18:27 -0400

    Clean up dependencies and remove unused files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:google_fonts/google_fonts.dart';
-
 import 'package:anonu/core/theme/app_theme.dart';
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-  GoogleFonts.config.allowRuntimeFetching = false;
-
-  test('dark theme is configured', () {
-    expect(AnonUTheme.dark.brightness.name, 'dark');
+  test('brutalist theme is configured properly', () {
+    expect(AnonUTheme.borderWidth, 3.0);
+    expect(AnonUTheme.popYellow, const Color(0xFFFFE600));
+    expect(AnonUTheme.popMint, const Color(0xFF00F090));
   });
 }
